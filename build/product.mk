5b8006e44f2c9461e083ba0b47e3f8d1adc2e97b
Enprytna <e.endipriyatna@gmail.com>
2022-03-31 09:40:14 +0000

    Revert "build: Set ro.apex.updatable=false in product properties"
    This reverts commit a018d22d366c44f41f191ddfcc159174fe08ebf5.

--- build/product.mk
+++ build/product.mk
@@ -23,9 +23,6 @@ $(call inherit-product-if-exists, vendor/proton/signing/dev.mk)
 
 # Flatten APEXs for performance
 OVERRIDE_TARGET_FLATTEN_APEX := true
-# This needs to be specified explicitly to override ro.apex.updatable=true from
-# prebuilt vendors, as init reads /product/build.prop after /vendor/build.prop
-PRODUCT_PRODUCT_PROPERTIES += ro.apex.updatable=false
 
 # Disable RescueParty due to high risk of data loss
 PRODUCT_PRODUCT_PROPERTIES += \
